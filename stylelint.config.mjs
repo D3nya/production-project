8404e2db873d4481537595ac6eb7e21d2975a403
Denis <51379629+D3nya@users.noreply.github.com>
2025-02-20 17:24:15 +0300

    fix prettier

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: ["stylelint-config-standard-scss", "stylelint-prettier/recommended"],
+  plugins: ["stylelint-prettier"],
   rules: {
     "selector-class-pattern": "^[a-z][a-zA-Z0-9]*$",
+    "prettier/prettier": [true, { endOfLine: "lf" }],
   },
 };
